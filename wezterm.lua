bd59d51b476368d638bcdfe579dfb342cbf02ea2
Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>
2024-07-15 07:06:08 +0800

    Update wezterm.lua
    Signed-off-by: Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>

--- wezterm.lua
+++ wezterm.lua
@@ -14,6 +14,14 @@ config.font = wezterm.font_with_fallback {
 -- For example, changing the color scheme:
 -- config.color_scheme = 'Cupcake (base16)'
 config.color_scheme = 'synthwave-everything'
+config.background = {
+  {
+    source = { File = '/home/caicai/图片/d5e286_4_makeup_4k.jpg' },
+    opacity = 0.4,
+  },
+}
+
+config.text_background_opacity = 0.8
 
 -- and finally, return the configuration to wezterm
 return config
