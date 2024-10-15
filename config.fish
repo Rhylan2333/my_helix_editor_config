6a0046fdbdbff56cedea683d03a9fff790ef052e
Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>
2024-10-15 21:18:35 +0800

    Update config.fish
    Signed-off-by: Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>

--- config.fish
+++ config.fish
@@ -1,15 +1,16 @@
-export PATH="$HOME/bin:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/helix-24.07-x86_64-linux:/usr/local/bin/kent_tools:/usr/local/bin/kent_tools/blat:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$HOME/.cargo/bin:/usr/local/bin/kent_tools:/usr/local/bin/kent_tools/blat:$PATH"
 
 export BAT_THEME="ansi"
 
 # RepeatMasker
-export PERL5LIB="/home/caicai/miniforge3/envs/TE-Annotation/share/RepeatMasker:$PERL5LIB"
+# export PERL5LIB="/home/caicai/miniforge3/envs/TE-Annotation/share/RepeatMasker:$PERL5LIB"
 
 export http_proxy="http://127.0.0.1:7890"
 export https_proxy="http://127.0.0.1:7890"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    eval "$(zellij setup --generate-auto-start fish | string collect)"
 end
 
 set -gx PNPM_HOME "/home/caicai/.local/share/pnpm"
@@ -18,18 +19,21 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 
 alias cr="clear"
-alias e="exa"
-alias et="exa -T"
-alias el="exa -hl"
-alias eal="exa -ahl"
+alias e="eza"
+alias et="eza -T"
+alias el="eza -hl"
+alias elt="eza -hlT"
+alias eal="eza -ahl"
 alias ma="mamba activate"
 alias mda="mamba deactivate"
 alias mel="mamba env list"
 alias bst="bat -S -l tsv"
+alias bs="bat -S"
+alias yz="yazi"
 
 starship init fish | source
 zoxide init fish | source
-thefuck --alias | source
+# thefuck --alias | source
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
