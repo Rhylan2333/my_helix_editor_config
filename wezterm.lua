64f3a7e151e0188ee6ad23d145b18954aab2d229
Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>
2025-09-03 00:23:50 +0800

    Update wezterm.lua
    Signed-off-by: Rhylan Cai <75231089+Rhylan2333@users.noreply.github.com>

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,17 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- This is where you actually apply your config choices.
 config.background = {
   {
-    source = { File = '/home/caicai/Pictures/d5e286_4_makeup_4k.jpg' },
+    source = { File = '/home/caicai/图片/瑟瑞斯.png' },
     horizontal_align = 'Center',
-    hsb = { brightness = 0.1 },
-    opacity = 1,
     vertical_align = 'Middle',
+    hsb = { brightness = 0.1 },
+    opacity = 0.9,
   },
 }
 
@@ -88,86 +91,23 @@ config.colors = {
   },
 }
 
--- config.color_scheme = 'Cupcake (base16)'
--- config.color_scheme = 'cyberpunk'
--- config.color_scheme = 'PhD (base16)'
--- config.color_scheme = 'synthwave-everything'
--- config.color_scheme = 'Bamboo Multiplex'
-config.color_scheme = 'CutiePro'
-config.color_scheme_dirs = { '$HOME/.config/wezterm/colors' }
-config.default_prog = { '/usr/bin/fish' }
--- config.default_prog = { '/usr/bin/fish', '-c', "zellij -l welcome --config-dir ~/.config/yazelix/zellij options --layout-dir ~/.config/yazelix/zellij/layouts" }
-
-
 config.enable_scroll_bar = true
-
-config.font = wezterm.font_with_fallback {
-  'JetBrainsMono Nerd Font Mono',
-  'Noto Sans CJK SC',
-}
-
--- -- The filled in variant of the ◖ symbol
--- local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
-
--- -- The filled in variant of the ◗ symbol
--- local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_right_half_circle_thick
-
--- -- This function returns the suggested title for a tab.
--- -- It prefers the title that was set via `tab:set_title()`
--- -- or `wezterm cli set-tab-title`, but falls back to the
--- -- title of the active pane in that tab.
--- function tab_title(tab_info)
---   local title = tab_info.tab_title
---   -- if the tab title is explicitly set, take that
---   if title and #title > 0 then
---     return title
---   end
---   -- Otherwise, use the title from the active pane
---   -- in that tab
---   return tab_info.active_pane.title
--- end
-
--- wezterm.on(
---   'format-tab-title',
---   function(tab, tabs, panes, config, hover, max_width)
---     local edge_background = '#ffffff'
---     local background = '#1b1032'
---     local foreground = '#808080'
-
---     if tab.is_active then
---       background = '#2b2042'
---       foreground = '#c0c0c0'
---     elseif hover then
---       background = '#3b3052'
---       foreground = '#909090'
---     end
-
---     local edge_foreground = background
-
---     local title = tab_title(tab)
-
---     -- ensure that the titles fit in the available space,
---     -- and that we have room for the edges.
---     title = wezterm.truncate_right(title, max_width - 1)
-
---     return {
---       { Background = { Color = edge_background } },
---       { Foreground = { Color = edge_foreground } },
---       { Text = SOLID_LEFT_ARROW },
---       { Background = { Color = background } },
---       { Foreground = { Color = foreground } },
---       { Text = title },
---       { Background = { Color = edge_background } },
---       { Foreground = { Color = edge_foreground } },
---       { Text = SOLID_RIGHT_ARROW },
---     }
---   end
--- )
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "NONE"
 
-config.text_background_opacity = 0.8
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 48
 
+-- or, changing the font size and color scheme.
+config.font = wezterm.font_with_fallback {
+  'Maple Mono NF CN',
+  'Noto Sans Mono CJK SC',
+}
+config.font_size = 13
+-- config.color_scheme = 'Brewer (base16)'
+
+-- Finally, return the configuration to wezterm:
 return config
